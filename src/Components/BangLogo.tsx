b0e95565c5419ae89f37eba9c911abd11d3cbfa9
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-10-21 18:35:01 -0400

    it should work now

--- src/Components/BangLogo.tsx
+++ src/Components/BangLogo.tsx
@@ -2,5 +2,5 @@ import './Style/BangLogo.css';
 import getLabel from "../Locale/GetLabel";
 
 export default function BangLogo() {
-    return <img className='bang-logo' src='logo128.png' alt={getLabel('ui', 'APP_TITLE')} />
+    return <img className='bang-logo' src='logo192.png' alt={getLabel('ui', 'APP_TITLE')} />
 }
