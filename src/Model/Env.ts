bcfd3b7a76a59910196820b24651979d3def8f28
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-10-30 16:43:25 -0400

    rework on the way I remove the paypal links

--- src/Model/Env.ts
+++ src/Model/Env.ts
@@ -3,6 +3,8 @@ export type Language = 'en' | 'it';
 const Env = (() => {
     const bangServerUrl = import.meta.env.VITE_BANG_SERVER_URL as string | undefined;
     const language = import.meta.env.VITE_LANGUAGE as Language | undefined;
+    const discordLink = import.meta.env.VITE_DISCORD_LINK as string | undefined;
+    const paypalDonateLink = import.meta.env.VITE_PAYPAL_DONATE_LINK as string | undefined;
 
     if (!bangServerUrl) {
         throw new Error('missing BANG_SERVER_URL environment variable');
