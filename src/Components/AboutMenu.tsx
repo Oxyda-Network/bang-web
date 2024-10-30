879b00d7d12268e6a5d9869195b01c0566c3ab57
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-10-30 18:33:18 -0400

    Trying to fix new menu

---
+++ src/Components/AboutMenu.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useLayoutEffect, useRef, useState } from "react";
+import "./Style/AboutMenu.css";
+
+export interface AboutMenuItemProps {
+  onClick: () => void;
+  children: ReactNode;
+}
+
+export function AboutMenuItem({ onClick, children }: AboutMenuItemProps) {
+  return <li><button onClick={onClick} className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">{children}</button></li>;
+}
+
+export interface AboutMenuProps {
+  children: ReactNode;
+}
+
+export default function AboutMenu({ username, setUsername, children }: AboutMenuProps) {
+  return (
+    <div className='about-menu z-50
+    absolute top-10 right-0
+    text-base list-none divide-y rounded-lg shadow bg-gray-700 divide-gray-600'>
+      <ul className="py-2" aria-labelledby="about-menu-button">{children}</ul>
+    </div>
+  )
+}
