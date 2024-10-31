1cec4eadf3c70cf8b280c2e6c0892f19217a3f04
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-10-31 02:17:14 -0400

    change library

--- src/Components/PdfViewer.tsx
+++ src/Components/PdfViewer.tsx
@@ -13,7 +13,7 @@ const PdfViewer: React.FC<PdfViewerProps> = ({ fileUrl, onClose }) => {
         <button onClick={onClose} className="absolute top-2 right-2 text-gray-600 hover:text-gray-900">
             Close
         </button>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.7.0/build/pdf.worker.min.js`}>
+        <Worker workerUrl={`https://cdn.jsdelivr.net/npm/pdfjs-dist@3.7.0/build/pdf.worker.min.js`}>
             <Viewer fileUrl={fileUrl} />
         </Worker>
         </div>
