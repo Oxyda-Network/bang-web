838c95729f3a8bbd06647e5aacc590b22ab3e863
DjBuRnOuT <1522047+DjBuRnOuT@users.noreply.github.com>
2024-10-31 22:39:54 -0400

    it's worst than it was

--- src/Components/PDFViewer.tsx
+++ src/Components/PDFViewer.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/4.7.76/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
 interface PDFViewerProps {
     file: string;
